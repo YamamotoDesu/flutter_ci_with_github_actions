dd0c9c91e5ec85be46f7241ba9cd8b14251f73d1
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-07-10 21:08:58 +0900

    chore: test CI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,6 +10,6 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
    test('Example test', () {
     // Insert your test code here
-    expect(2 + 3, 5); // Example assertion
+    expect(2 + 2, 4); // Example assertion
   });
 }
